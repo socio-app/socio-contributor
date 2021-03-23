3285376242d73d9dc9f4621914126c9394a79e86
okipratama97 <okipratama97@gmail.com>
2021-03-24 01:02:08 +0700

    fixed bug

--- src/store/actions.js
+++ src/store/actions.js
@@ -24,6 +24,7 @@ export function postMission(payload) {
       swalLoading()
 
       const response = await axios({
+        method: 'post',
         url: `/missions`,
         data: mission,
       })
